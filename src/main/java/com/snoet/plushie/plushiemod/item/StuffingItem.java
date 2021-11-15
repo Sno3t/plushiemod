e276a0cce0bf2d33143c54881727f1a5d02b7105
Sno3t <tycho.brakenhoff@ictmbo.nl>
2021-11-15 20:20:50 +0100

    Init

---
+++ src/main/java/com/snoet/plushie/plushiemod/item/StuffingItem.java
@@ -0,0 +1,12 @@
+package com.snoet.plushie.plushiemod.item;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.registries.ForgeRegistry;
+
+import java.util.List;
+
+public class StuffingItem extends Item {
+
+
+}
